f7cc215299470a41b614ee629a88f7b1f75f7d21
aproragadozo <aproragadozo@gmail.com>
2019-08-04 15:26:52 +0200

    init

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # german-ipsum
 
 ## Project setup
-```
+
+```shell
 npm install
 ```
 
 ### Compiles and hot-reloads for development
-```
+
+```shell
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+
+```shell
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
--- src/components/IpsumWrapper.vue
+++ src/components/IpsumWrapper.vue
@@ -5,29 +5,46 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
-    <div class="controls">
-      <input type="range" id="start" name="volume"
-         min="0" max="11">
-  <label for="volume">Volume</label>
-  <div data-role="fieldcontain">
-    <fieldset data-role="controlgroup" data-type="horizontal">
-	    		<legend>HTML tag Wrapper</legend>
+    <div class="controls" data-role="fieldcontain">
+      <fieldset data-role="controlgroup" data-type="horizontal">
+        <label for="volume" style="display: inline-block; position:relative;">Number of sentences</label>
+        <input type="range" id="start" name="volume" min="1" max="11" step="1" list="sentences">
 
-		     	<input type="radio" name="radio-choice-1" id="radio-choice-1" value="choice-1" checked="checked" />
-		     	<label for="radio-choice-1">No Wrapper</label>
-
-		     	<input type="radio" name="radio-choice-1" id="radio-choice-2" value="choice-2" />
-		     	<label for="radio-choice-2">&lt;li&gt;</label>
-
-		     	<input type="radio" name="radio-choice-1" id="radio-choice-3" value="choice-3" />
-		     	<label for="radio-choice-3">&lt;p&gt;</label>
-	    	</fieldset>
-  </div>
+        <datalist id="sentences">
+          <option value="1" label="1"/>
+          <option value="2" label="2"/>
+          <option value="3" label="3"/>
+          <option value="4" label="4"/>
+          <option value="5" label="5"/>
+          <option value="6" label="6"/>
+          <option value="7" label="7"/>
+          <option value="8" label="8"/>
+          <option value="9" label="9"/>
+          <option value="10" label="10"/>
+        </datalist>
+      </fieldset>
+      <fieldset data-role="controlgroup" data-type="horizontal">
+        <legend>HTML tag Wrapper</legend>
+        <input type="radio"
+          name="radio-choice-1"
+          id="radio-choice-1" value="choice-1" checked="checked" />
+        <label for="radio-choice-1">No Wrapper</label>
+        <input type="radio" name="radio-choice-1" id="radio-choice-2" value="choice-2" />
+        <label for="radio-choice-2">&lt;li&gt;</label>
+        <input type="radio" name="radio-choice-1" id="radio-choice-3" value="choice-3" />
+        <label for="radio-choice-3">&lt;p&gt;</label>
+      </fieldset>
     </div>
     <div class="go">
       <p>Mach schnell!</p>
     </div>
-    <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@pear?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Pierre Gui"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Pierre Gui</span></a>
+    <a class="credit"
+      href="https://unsplash.com/@pear?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Image credit: Unsplash">
+      <span style="display:inline-block;padding:2px 3px">Pierre Gui</span>
+    </a>
   </div>
 </template>
 
@@ -44,28 +61,28 @@ export default {
   background-image: url("../assets/snack.jpg");
   background-position: center;
   background-size: cover;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
 }
-  .title {
-    font-size: 4em;
+.title {
+    font-size: 4vmax;
     text-transform: uppercase;
     color: whitesmoke;
 }
-  .box {
+.box {
     max-height: 50vmax;
     width: 50vw;
     background: rgba(208,204,151,0.6);
-display: flex;
-flex-direction: column;
-justify-content: space-around;
-align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
 }
-  .box>p {
+.box>p {
     opacity: 1;
-    font-size: 18px;
+    font-size: 1.5vmax;
     color:midnightblue;
     width: 80%;
 }
@@ -74,16 +91,26 @@ align-items: center;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  background:crimson;
   width: 50vw;
   justify-self: flex-end;
-  min-height: 10vh;
+  min-height: 5vmin;
+}
+fieldset {
+  border: none;
+  position: relative;
+  padding: 1em 0 1em 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  background:#dc143c99;
 }
 .go {
   text-transform: uppercase;
   background: rgb(32,184,120);
   color: whitesmoke;
-  font-size: 2em;
+  font-size: 2vmax;
   border-radius: 10px;
   display: flex;
   justify-content: center;
@@ -93,5 +120,17 @@ align-items: center;
 .go>p {
   width: 80%;
   text-align: center
+}
+a.credit{
+  background-color:#333;
+  color:white;
+  text-decoration:none;
+  padding:1px 3px;
+  font-family:-apple-system, Helvetica, Arial, sans-serif;
+  font-size:1.5vmin;
+  font-weight:bold;
+  line-height:1.2;
+  display:inline-block;
+  border-radius:3px;
 }
 </style>
